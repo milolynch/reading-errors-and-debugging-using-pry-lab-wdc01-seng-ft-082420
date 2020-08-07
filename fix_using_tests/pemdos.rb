01a11c87405cb0c8df89653d332cf953305aee91
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-07 18:11:25 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,15 @@
 # don't forget to add: require 'pry'
 
+require 'pry'
+
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    x = 0
+    while x < 10 do
+      string = "s" + string
+      x = x + 1
+    end
+    return string 
   else
     string
   end
